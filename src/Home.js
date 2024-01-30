b9de8d9f91d15537e8a224efd25a650cae06dc78
Yashasvi <kmryashasvi@gmail.com>
2024-01-30 13:46:45 +0530

    added favicon and disappearing song name

--- src/Home.js
+++ src/Home.js
@@ -144,7 +144,7 @@ const Home = () => {
       {/* Render filter options */}
       <div className='filters'>
           <div className='title-bar'>
-              <h3 className='title'>The Minimalist Song Player</h3>
+              <h1 className='title'>Yash FM</h1>
               <button className='title-button' data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 550 550"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
               </button>
@@ -199,7 +199,7 @@ const Home = () => {
 
       <div className='player row text-center pt-2 m-0'>
 
-        <div className='songName'>
+        <div className='songName' style={isPlaying?{opacity:1}:{opacity:0}}>
           <div className="loading-wave">
             <div className="loading-bar"></div>
             <div className="loading-bar"></div>
